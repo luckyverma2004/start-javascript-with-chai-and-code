bf847d604bb07d031fee01ac681c4e25ca2df081
lucky verma <lv6678082@gmail.com>
2025-01-25 10:52:03 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,25 @@
+let score = "33sbj"
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+// these are the conversion jab hum in chizo ko convert karte hai to for ex!
+//"33"= 33
+//"33ab"= NAN
+// true => 1 or false => 0
+
+let IsloggedIn = ""
+
+let booleanIsloggedin = Boolean (IsloggedIn)
+//console.log(booleanIsloggedin);
+
+// 1 => true and 0 => false
+// "" => false
+// "lucly"  => true
+
+let someNumber = 77
+
+let stringNumber = string(someNumber)
+console.log (stringNumber);
