02437203f4d3b8e01e9ae0d974931f5dff4b1fd4
lucky verma <lv6678082@gmail.com>
2025-01-25 07:26:20 +0000

    datatypes

---
+++ 02_datatypes.js/test.js
@@ -0,0 +1,36 @@
+"use strict"; //treat all js code as a new vcersion
+
+// alert (3+3) // beacuse we are use node js not a browser
+
+/*console.log (
+    3+3) // they are not readable because readability is must be important
+
+    console.log("lucky")*/
+
+    console.log(3+3)
+    
+    console.log("lucky") // these are the proper way of readability
+
+    // datatypes in javascript // inko yad karne ka tarika NNSSBB
+
+    //number => 9098
+    //string => "lucky"
+    //boolean => true ya false
+    //null => mtlab khali jagah
+    //undefiend => ka matalab undefiend
+    //bigint => use in big wabsite for jyada numbers ke liye
+    // symbol => uniquly identified ke liye
+
+    // object
+
+    let name = "lucky"
+    let age = 18
+    let islogdin = false
+     
+    // ager hame kisi ka bhi datatype pata lagana hai to hum type of use karenge
+
+    console.log(typeof age ); // number
+    console.log(typeof name);// string
+    console.log(typeof islogdin);// true ya false
+    console.log(typeof null);//object
+    console.log(typeof undefined);//undefined
