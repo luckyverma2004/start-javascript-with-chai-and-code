a6943128738d10443c76f78cf5d2bad3f55024c8
lucky verma <lv6678082@gmail.com>
2025-01-27 14:46:57 +0000

    heap&stack

---
+++ 01_basics/heap.js
@@ -0,0 +1,31 @@
+// heap (Primitive) and stack (non-primitive)
+
+//**************This was a example of stack******************* */
+
+let MyInstagramName = "luckyverma545"
+
+let anotherName = "MyInstagramName"
+anotherName = "LuckyjiVerma"
+
+console.log(MyInstagramName);
+console.log(anotherName);
+
+//***************This was a example of heap*****************************/
+
+let userOne = {
+    email:  "lv6678082@gmail.com"
+    
+}
+
+let userTwo = userOne
+
+userTwo.email = "luckyverma@2004"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+    
+
+
+ 
