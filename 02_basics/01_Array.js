ffa89070edd2345c2769444880eb7873acab2dfd
lucky verma <lv6678082@gmail.com>
2025-02-01 09:28:31 +0000

    part - 1 array

---
+++ 02_basics/01_Array.js
@@ -0,0 +1,49 @@
+// Array - ye array ko describe karne ka tarika
+
+const myArr = [0,1,2,3,4,5] // array ko hamesha ye[] ye wale bracket se hi describe karte hai 
+const myHeroes = ["spiderman","badmen"]
+//console.log(myArr[1]);
+
+// Array marthods
+
+// myArr.push(6)
+// console.log(myArr);// ye push method  aaray mai add  karta hai value ko
+// myArr.pop(6)
+// console.log(myArr);// ye pop method remove karta hai 
+
+myArr.unshift(6)
+ console.log(myArr);// ye unshift method starting main shift kar deta hai array ke
+
+myArr.shift(5)
+ console.log(myArr);// ye shift method remove karta hai jisse hamne unshift ka use karke set kiya tha 
+
+// console.log(myArr.includes(9))//ye include mathod ka use karke hum swal puch sakte hai string se output--(true or false)-- main aayega 
+// console.log(myArr.indexOf(4));// ye hame batata hai ki string main ye hai ya nhi
+
+// const NewArr = myArr.join()// ye aaray ko bind karke string main convert kar deta hai
+// console.log(myArr);
+// console.log(NewArr);
+// console.log(typeof NewArr);
+
+// slice , spice
+
+// console.log("A",myArr)
+// const myn1 = myArr.slice(1,3)// slice main hame jis range main output chaiye vo hi print karate hai
+
+// console.log(myn1);
+
+// console.log("B",myArr);
+
+// const myn2 = myArr.splice(1,3)// ye sirf utna hi print kaerayega jitn tumhe chaiye 
+// console.log("c",myArr);
+
+// console.log(myn2);
+
+
+
+
+
+
+
+
+
