97aea1a7d1bbb666fe1bd041015c938fef849f6c
lucky verma <lv6678082@gmail.com>
2025-02-18 08:42:00 +0000

    functions 2

---
+++ 03_basics/02_functions.js
@@ -0,0 +1,9 @@
+// functions with array
+const myNewArray = [200,400,600,800]
+
+function returnsecondvalue(getArray){
+return getArray[1]
+}
+//console.log(returnsecondvalue(myNewArray));
+
+//console.log(returnsecondvalue([200,400,600]));
